1027f8023c837a8539b4f8e3722058549baeb6a1
Ederson Barros <ederson.barros@aa.com>
2025-04-29 16:46:16 -0500

    initial push

---
+++ src/main/java/com/in28minutesspring/learn_spring_boot/CourseController.java
@@ -0,0 +1,18 @@
+package com.in28minutesspring.learn_spring_boot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class CourseController {
+
+	@RequestMapping("/courses")
+	public List<Course> RetrieveAllCourses() {
+
+		return Arrays.asList(new Course(1, "Learn AWS", "in28minutes"), new Course(2, "Learn DevOps", "in28minutes"));
+	}
+}
